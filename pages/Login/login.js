0c7f8e4b8fc1609c978461552c2558c5ab55628f
陈宁伟 <chenningwei1030@gmail.com>
2019-10-30 16:34:32 +0800

    文档待完善

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -1,24 +1,26 @@
 // pages/Login/login.js
 Page({
 
-  Wechat_click: function(){
+    Wechat_click: function() {
 
-  console.log('微信登录')
+        console.log('微信登录')
 
     },
 
-    account_click:function(){
+    account_click: function() {
 
-      console.log('账号登陆')
+        console.log('账号登陆')
     },
- 
-    agreement: function(){
 
-      wx.navigateTo({
-        url: '/pages/Agreement/agreement',
-        
-      });
-  
+    agreement: function() {
 
-    }
+        wx.navigateTo({
+            url: '/pages/Agreement/agreement',
+
+        });
+    },
+
+
+
+
 })
--- pages/Profile/profile.js
+++ pages/Profile/profile.js
@@ -1,9 +1,62 @@
 // pages/Profile/profile.js
 Page({
 
-  LoginButtonClick:function(){
-    console.log('点击了登录按钮')
-  }
+    LoginButtonClick: function() {
+        console.log('点击了登录按钮')
+    },
+    // 列表
+    // data: {
 
+    //     listData: [
+    //         '123',
+    //         '345',
+    //         '123',
+    //         '456',
+
+
+    //     ]
+
+
+    // }
+
+    data: {
+        goodsArray: [{
+                title: '颜域蝴蝶结荷叶领雪纺连衣裙女夏时尚高端斑点裙子2018年新款热销',
+                desc: '黑点 M码',
+                price: '￥999.0',
+                num: '2'
+            },
+            {
+                title: '颜域蝴蝶结荷叶领雪纺连衣裙女夏时尚高端斑点裙子2018年新款热销',
+                desc: '黑点 M码',
+                price: '￥999.0',
+                num: '2'
+            },
+            {
+                title: '颜域蝴蝶结荷叶领雪纺连衣裙女夏时尚高端斑点裙子2018年新款热销',
+                desc: '黑点 M码',
+                price: '￥999.0',
+                num: '2'
+            },
+            {
+                title: '颜域蝴蝶结荷叶领雪纺连衣裙女夏时尚高端斑点裙子2018年新款热销',
+                desc: '黑点 M码',
+                price: '￥999.0',
+                num: '2'
+            },
+            {
+                title: '颜域蝴蝶结荷叶领雪纺连衣裙女夏时尚高端斑点裙子2018年新款热销',
+                desc: '黑点 M码',
+                price: '￥999.0',
+                num: '2'
+            }
+        ]
+    },
+    canvasIdErrorCallback: function(e) {
+        console.error(e.detail.errMsg)
+    },
+    onReady: function(e) {
+        this.goodsList = this.selectComponent("#goodsList");
+    }
 
 })
