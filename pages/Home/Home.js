fd1813aa1e129b34f571fa013dc67c06c56465e9
陈宁伟 <chenningwei1030@gmail.com>
2019-11-05 20:04:58 +0800

    初始化01

---
+++ pages/Home/Home.js
@@ -0,0 +1,19 @@
+// pages/Home/Home.js
+Page({
+
+    wx.showToast({
+        title: '',
+        icon: 'none',
+        image: '',
+        duration: 1500,
+        mask: false,
+        success: (result) => {
+
+        },
+        fail: () => {},
+        complete: () => {}
+    })
+
+
+
+})
--- pages/Profile/profile.js
+++ pages/Profile/profile.js
@@ -3,39 +3,57 @@ Page({
 
     LoginButtonClick: function() {
         console.log('点击了登录按钮')
+            // 获取地理位置
+        wx.getLocation({
+            type: 'wgs84',
+            altitude: false,
+            success: (result) => {
+
+                const latitude = result.latitude;
+                const longitude = result.longitude;
+                const speed = result.speed;
+                const accracy = result.accuracy; //位置的精确度
+
+
+            },
+            fail: () => {},
+            complete: () => {}
+        });
+
+
+
     },
     // 列表
     data: {
-        items: [
-            {
-              icon: 'ic_cat_movie.png',
-              title: '观影分析',
-              count: 0,
-              has: '看过',
-              mark: '标记10部影片\n开启观影分析'
+        items: [{
+                icon: 'ic_cat_movie.png',
+                title: '观影分析',
+                count: 0,
+                has: '看过',
+                mark: '标记10部影片\n开启观影分析'
             },
             {
-              icon: 'ic_cat_book.png',
-              title: '读书分析',
-              count: 0,
-              has: '读过',
-              mark: '标记10本书\n开启读书分析'
+                icon: 'ic_cat_book.png',
+                title: '读书分析',
+                count: 0,
+                has: '读过',
+                mark: '标记10本书\n开启读书分析'
             },
             {
-              icon: 'ic_cat_music.png',
-              title: '音乐分析',
-              count: 0,
-              has: '听过',
-              mark: '标记10张唱片\n开启观音乐分析'
+                icon: 'ic_cat_music.png',
+                title: '音乐分析',
+                count: 0,
+                has: '听过',
+                mark: '标记10张唱片\n开启观音乐分析'
             }
-          ]
+        ]
     },
 
-    beginAction:function(evt){
+    beginAction: function(evt) {
 
         console.log(evt)
 
     }
-   
+
 
 })
